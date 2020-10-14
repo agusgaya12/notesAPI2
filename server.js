563bba8f8d04e3d78ee6a0e8e618ef34190e875d
agusgaya12 <ag.rifai12@gmail.com>
2020-10-14 18:32:43 +0700

    adding Route into server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const addNoteRoute = require('./Modules/postNotes')
+const editNoteRoute = require('./Modules/patchNotes')
+const deleteNoteRoute = require('./Modules/DeleteNotes')
+const getNoteRoute = require('./Modules/getnNotes')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(addNoteRoute)
+app.use(editNoteRoute)
+app.use(deleteNoteRoute)
+app.use(getNoteRoute)
+
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
